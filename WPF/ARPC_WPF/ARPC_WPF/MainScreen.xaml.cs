7afb1992ff9289f9d464fc1881f831774517218a
danielalexe <daniel.alexe@outlook.com>
2019-07-02 19:11:53 +0300

    #5 - WPF - Tab based central display

--- WPF/ARPC_WPF/ARPC_WPF/MainScreen.xaml.cs
+++ WPF/ARPC_WPF/ARPC_WPF/MainScreen.xaml.cs
@@ -11,6 +11,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using ARPC_WPF.Profesori;
+using ARPC_WPF.Template;
 using MaterialDesignThemes;
 
 namespace ARPC_WPF
@@ -20,6 +22,7 @@ namespace ARPC_WPF
     /// </summary>
     public partial class MainScreen : Window
     {
+        int childCount = 0;
         public MainScreen()
         {
             InitializeComponent();
@@ -40,6 +43,39 @@ namespace ARPC_WPF
         {
             ButtonOpenDrawer.Visibility = Visibility.Visible;
             ButtonCloseDrawer.Visibility = Visibility.Collapsed;
+        }
+
+        private void CreateTabItem(WindowBase window)
+        {
+            //child window will now hold a reference value to the tab control
+
+            window.TabCtrl = TabControlMain;
+
+            //Add a Tabitem and enables it
+            TabItem tp = new TabItem();
+            tp.Header = window.Title;
+            tp.Content = window.Content;
+            TabControlMain.Items.Add(tp);
+
+            //child Form will now hold a reference value to a tabitem
+            window.TabItem = tp;
+
+            //Activate the MDI child form
+
+            childCount++;
+
+            //Activate the newly created Tabitem
+            TabControlMain.SelectedItem = tp;
+        }
+
+        private void ListViewItem_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            var item = sender as ListViewItem;
+            if (item!=null)
+            {
+                WindowProfesoriColectie wind = new WindowProfesoriColectie();
+                CreateTabItem(wind);
+            }
         }
     }
 }
---
+++ WPF/ARPC_WPF/ARPC_WPF/Profesori/WindowProfesoriColectie.xaml.cs
@@ -0,0 +1,33 @@
+﻿using ARPC_WPF.Template;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ARPC_WPF.Profesori
+{
+    /// <summary>
+    /// Interaction logic for WindowProfesoriColectie.xaml
+    /// </summary>
+    public partial class WindowProfesoriColectie : WindowBase
+    {
+        public WindowProfesoriColectie()
+        {
+            InitializeComponent();
+        }
+
+        private void ButonClose_Click(object sender, RoutedEventArgs e)
+        {
+            this.TabCtrl.Items.Remove(this.TabItem);
+        }
+    }
+}
