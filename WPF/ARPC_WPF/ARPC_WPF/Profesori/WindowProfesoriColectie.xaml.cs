6f8f8bc75daa9069a9b27f6aa8193980a4632936
danielalexe <daniel.alexe@outlook.com>
2019-07-04 23:53:50 +0300

    #6 - WPF - TopControlBar (Close, ....)

--- WPF/ARPC_WPF/ARPC_WPF/Profesori/WindowProfesoriColectie.xaml.cs
+++ WPF/ARPC_WPF/ARPC_WPF/Profesori/WindowProfesoriColectie.xaml.cs
@@ -1,4 +1,6 @@
-﻿using ARPC_WPF.Template;
+﻿using ARPC_WPF.Data;
+using ARPC_WPF.Template;
+using DataObjects;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -23,11 +25,70 @@ namespace ARPC_WPF.Profesori
         public WindowProfesoriColectie()
         {
             InitializeComponent();
+            ReloadData();
         }
 
         private void ButonClose_Click(object sender, RoutedEventArgs e)
         {
             this.TabCtrl.Items.Remove(this.TabItem);
+        }
+
+        private void ButtonRefresh_Click(object sender, RoutedEventArgs e)
+        {
+            ReloadData();
+        }
+
+        private void ReloadData()
+        {
+            using (var db = new ARPCContext())
+            {
+                var getProfesori = (from u in db.PROFESORIs
+                                    select new DTOProfesori
+                                    {
+                                        ID_PROFESOR = u.ID_PROFESOR,
+                                        NUME = u.NUME,
+                                        PRENUME = u.PRENUME
+                                    }).ToList();
+                DataGridProfesori.ItemsSource = getProfesori;
+            }
+        }
+
+        private void ButtonDelete_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ButtonEdit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ButtonAdd_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+
+        private void RenderColumns()
+        {
+            foreach (DataGridColumn c in DataGridProfesori.Columns)
+            {
+                if (c.Header.ToString().StartsWith("ID_") || c.Header.ToString().StartsWith("PASSWORD") || c.Header.ToString().StartsWith("PAROLA"))
+                    c.Visibility = Visibility.Collapsed;
+                String aux = c.Header.ToString().ToLower();
+                c.Header = ReplaceFirstCharacterFromString(aux);
+            }
+        }
+
+        public string ReplaceFirstCharacterFromString(string text)
+        {
+            String aux = String.Format("{0}{1}", text.First().ToString().ToUpperInvariant(), text.Substring(1));
+            return aux.Replace('_', ' ');
+        }
+
+        private void Grid_Loaded(object sender, RoutedEventArgs e)
+        {
+            RenderColumns();
         }
     }
 }
---
+++ WPF/ARPC_WPF/DataObjects/DTOProfesori.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataObjects
+{
+    public class DTOProfesori
+    {
+        public int ID_PROFESOR { get; set; }
+        public string NUME { get; set; }
+        public string PRENUME { get; set; }
+    }
+}
